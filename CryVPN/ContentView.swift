a50b392f5c7867eab8ee56793900e42453c3ae2a
crymoooreee <sashapeshin2004@yandex.ru>
2025-11-14 14:44:29 +0500

    Initial commit  CryVPN project

---
+++ CryVPN/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("CryVPN")
+                    .font(.largeTitle)
+                Text("Demo Project Structure")
+            }
+            .padding()
+        }
+    }
+}
---
+++ CryVPN/CryVPNApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct CryVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
